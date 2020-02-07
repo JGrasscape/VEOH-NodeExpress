c78e788225cec97b7a5956f5b6a6f801d6dd7449
Ruohoniemi Janne <99800597116@teonet.org>
2020-02-07 17:23:49 +0200

    controllers

--- app-08-organize-controllers.js
+++ app-08-organize-controllers.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const PORT = process.env.PORT || 8080;
 const body_parser = require('body-parser');
-const session = require('express-session'); // terminal: npm install express-session
-const mongoose = require('mongoose'); // terminal: npm install mongoose
-//const Schema = mongoose.Schema;
+const session = require('express-session');
+const mongoose = require('mongoose');
 
-// Controllers
+//Controllers
 const auth_controller = require('./controllers/auth_controller');
-
-// Models
-const user_model = require('./models/user-model');
-const note_model = require('./models/note-model');
-
-// Views
-const auth_views = require('./views/auth-views');
-const note_views = require('./views/note-views');
+const note_controller = require('./controllers/note_controller');
 
 let app = express();
 
@@ -32,118 +24,49 @@ app.use(session({
 }));
 
 app.use((req, res, next) => {
-    console.log('PATH: ' + req.path);
+    console.log(`${req.method} ${req.path}`);
     next();
 });
 
 const is_logged_handler = (req, res, next) => {
-    // Ei kirjautunutta käyttäjää
     if (!req.session.user) {
         return res.redirect('/login');
     }
     next();
 };
 
-// Auth
+//Serve Static files
+app.use('/css', express.static('css'))
+
+//Auth
+app.use(auth_controller.handle_user);
 app.get('/login', auth_controller.get_login);
-app.post('/logout', auth_controller.post_logout);
 app.post('/login', auth_controller.post_login);
 app.post('/register', auth_controller.post_register);
+app.post('/logout', auth_controller.post_logout);
 
-// Haetaan käyttäjän tietokanta-objekti
-app.use((req, res, next) => {
-    if (!req.session.user) {
-        return next();
-    }
-    user_model.findById(req.session.user._id).then((user) => {
-        req.user = user;
-        next();
-    }).catch((err) => {
-        console.log(err);
-        res.redirect('/login');
-    });
-});
 
-app.get('/', is_logged_handler, (req, res, next) => {
-    //const user = req.session.user;
-    const user = req.user;
-    // Popouloidaan userin notes-lista ja renderöidään sivu
-    user.populate('notes').execPopulate().then(() => {
-        console.log('user:', user);
-        //res.send(note_views.notes_view({
-        let data = {
-            user_name: user.name,
-            notes: user.notes
-        }
-        //}));
-        let html = note_views.notes_view(data);
-        res.send(html);
-    });
-});
-
-app.post('/add_note', (req, res, next) => {
-    const user = req.user;
-
-    let new_note = note_model({
-        text: req.body.note
-    });
-    new_note.save().then(() => {
-        console.log('note saved');
-        user.notes.push(new_note); // Lisätään viittaus käyttäjäobjektin notes-listaan
-        user.save().then(() => {
-            console.log('user saved');
-            return res.redirect('/');
-        });
-    });
-});
-
-app.post('/delete_note', (req, res, next) => {
-    const user = req.user;
-    const note_id_to_delete = req.body.note_id; // hidden inputin name
-
-    // Remove note from user.notes
-    const updated_notes = user.notes.filter((note_id) => { // Käy listaa läpi
-        // Palauttaa true or false;
-        // Jos true, häviää listalta
-        return note_id != note_id_to_delete;
-    });
-    user.notes = updated_notes;
-
-    user.save().then(() => {
-        // Käyttäjät päivitetty, viittaus notesiin poistettu. Itse note vielä olemassa kannassa.
-        // Poistetaan itse note.
-        note_model.findByIdAndRemove(note_id_to_delete).then(() => {
-            res.redirect('/');
-        });        
-    });
-});
-
-// Yksittäisen noten haku
-app.get('/note/:id', (req, res, next) => {
-    const note_id = req.params.id;
-    note_model.findOne({
-        _id: note_id
-    }).then((note) => {
-        res.send(note.text);
-    });
-});
+//Notes
+app.get('/', is_logged_handler, note_controller.get_notes);
+app.post('/delete-note', is_logged_handler, note_controller.post_delete_note);
+app.get('/note/:id', is_logged_handler, note_controller.get_note);
+app.post('/add-note', is_logged_handler, note_controller.post_note);
 
 app.use((req, res, next) => {
-    console.log('404');
     res.status(404);
-    res.send('404');
-    res.end();
+    res.send(`
+        page not found
+    `);
 });
 
-// yhteys tietokantaan
-// 1X5P8Y6oTPpOfpms
-// mongodb+srv://db-user:<password>@cluster0-zf1di.mongodb.net/test?retryWrites=true&w=majority
+//Shutdown server CTRL + C in terminal
+//const mongoose_url = 'mongodb+srv://db-user:1KRO2OhneATkq0Ke@cluster0-soknu.mongodb.net/test?retryWrites=true&w=majority';
 const mongoose_url = 'mongodb+srv://db-user:1X5P8Y6oTPpOfpms@cluster0-zf1di.mongodb.net/test?retryWrites=true&w=majority';
+
 mongoose.connect(mongoose_url, {
     useUnifiedTopology: true,
     useNewUrlParser: true
 }).then(() => {
-    // serverin käynnistys
     console.log('Mongoose connected');
     console.log('Start Express server');
     app.listen(PORT);
--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -1,6 +1,20 @@
 const user_model = require('../models/user-model');
 const auth_views = require('../views/auth-views');
 
+// Haetaan käyttäjän tietokanta-objekti
+const handle_user = (req, res, next) => {
+    if (!req.session.user) {
+        return next();
+    }
+    user_model.findById(req.session.user._id).then((user) => {
+        req.user = user;
+        next();
+    }).catch((err) => {
+        console.log(err);
+        res.redirect('/login');
+    });
+};
+
 const get_login = (req, res, next) => {
     console.log('user:', req.session.user);
     res.send(auth_views.login_view());
@@ -51,3 +65,4 @@ module.exports.get_login = get_login;
 module.exports.post_logout = post_logout;
 module.exports.post_login = post_login;
 module.exports.post_register = post_register;
+module.exports.handle_user = handle_user;
